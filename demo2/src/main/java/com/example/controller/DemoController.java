25c9c3faac38740d5756272878237b195dc4774f
zeg <15311243981@163.com>
2018-11-27 11:43:35 +0800

    Initial commit

---
+++ demo2/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: DemoController
+ * @Decription: TODO
+ * @Author: ZEG
+ * @date: 2018/11/27 11:28
+ */
+@RestController
+@EnableAutoConfiguration
+public class DemoController {
+    @RequestMapping("getString")
+    public String getString(){
+        return "getString";
+    }
+}
